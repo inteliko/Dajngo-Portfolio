1a0194eb95f630b5c743d628477ebbfea836b1ae
riad <maksusbinislam@gmail.com>
2023-08-31 22:04:35 +0600

    add

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,8 @@ echo 'Building the project...'
 pip install -r requirements.txt
 
 echo "Make migration"
-
-python3.10.2 manage.py makemigration --noinput
-python3.10.2 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "collect static"
-python3.10.2 manage.py collectstatic --noinput --clear
-
+python3 manage.py collectstatic --noinput --clear
